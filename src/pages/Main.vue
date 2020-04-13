2a1e11215f7b6ba69124f42e1a95f44db0088fec
Tron <congzhang@dh-data.com>
2020-04-13 17:26:33 +0800

    edit

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,13 +1,43 @@
 <template>
   <div class="enterMain">
-    主页-main
-    <a-button>跳转到登录</a-button>
+    <div class="progress">
+      <div class="click_block" v-for="i in progress" v-bind:key="i" @mousedown="(e)=>handlerMousedown(i, e)" >
+        <span class="item" />
+      </div>
+    </div>
+    <div class="maskModal" :style="`display: ${maskModal?'block':'none'}`" @mousemove="handlerMousemove" @mouseup="handlerMouseup" />
+    <!-- 主页-main
+    <a-button>跳转到登录</a-button> -->
   </div>
 </template>
 
 <script>
 export default {
   name: 'enterMain',
+  data(){
+    return {
+      progress: [1,2,3,4,5,6,7,8,9,10],
+      maskModal: false,
+      clickX: 0,
+      innerWidth: window.innerWidth
+    }
+  },
+  methods: {
+    handlerMousedown(i, e){ // 鼠标按下 
+      console.log(`当前点击第${i}个, 横向坐标${e.screenX}, 页面总宽度${this.innerWidth}`)
+      this.maskModal = true
+      this.clickX = e.screenX
+    },
+    handlerMousemove(e){ // 鼠标移动 
+      let moveX = e.screenX // 鼠标移动的位置
+      let onePercent = this.innerWidth/100 // 百分之一视窗大小的宽度
+      console.log(`当前x坐标轴位置:${moveX}, 相对点击时x坐标位置:${this.clickX - moveX}, 相对整个网页视窗移动百分比:${((this.clickX - moveX)/onePercent).toFixed(2)}%`)
+      // todo 
+    },
+    handlerMouseup(){ // 鼠标放开
+      this.maskModal = false
+    }
+  },
   mounted() {
     console.log('enterMain')
   }
@@ -16,5 +46,32 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less" scoped>
-
+.enterMain{
+  padding: 24px;
+  .progress{
+    cursor: pointer;
+    width: 200px;
+    margin: 0 auto;
+    display: flex;;
+    .click_block{
+      flex: 1;
+        text-align: center;
+      .item{
+        display: inline-block;
+        width: 10px;
+        height: 20px;
+        background-color: blue;
+      }
+    }
+  }
+  .maskModal{
+    position: absolute;
+    pointer-events: none;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, .1);
+  }
+}
 </style>
